45596c0d66709be5e7c668b20977d653cd69d2a6
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2024-05-29 15:39:13 +0330

    update card

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,48 @@ function App() {
   
 
   return (
-   <div>
+   <div className='flex flex-col gap-[6rem]'>
     <Card
     color="#FF8B64"
     icon="/images/icon-work.svg"
     title="Work"
     time="32hrs"
     text="Last Week - 36hrs"
-
+    />
+    <Card
+    color="#55C2E6"
+    icon="/images/icon-play.svg"
+    title="Play"
+    time="10hrs"
+    text="Last Week - 36hrs"
+    />
+    <Card
+    color="#FF5E7D"
+    icon="/images/icon-study.svg"
+    title="Study"
+    time="4hrs"
+    text="Last Week - 7hrs"
+    />
+    <Card
+    color="#4BCF82"
+    icon="/images/icon-exercise.svg"
+    title="Exercise"
+    time="4hrs"
+    text="Last Week - 5hrs"
+    />
+    <Card
+    color="#7335D2"
+    icon="/images/icon-social.svg"
+    title="Social"
+    time="5hrs"
+    text="Last Week - 10hrs"
+    />
+    <Card
+    color="#F1C75B"
+    icon="/images/icon-self-care.svg"
+    title="Self Care"
+    time="2hrs"
+    text="Last Week - 2hrs"
     />
    </div>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Card({ color, icon, title, text, time }) {
   return (
-    <div>
+    <div className="relative">
       <div
         style={{ backgroundColor: color }}
         className="w-[20rem] h-[6rem] rounded-xl"
       >
         <img src={icon} className="ml-auto mr-4" />
       </div>
-      <div className="text-white bg-[#1C204B] w-[20rem] h-[8rem] absolute top-12 rounded-xl p-4 flex flex-col justify-center gap-6">
+      <div className="text-white bg-[#1C204B] w-[20rem] h-[8rem] rounded-xl p-4 flex flex-col justify-center gap-6 absolute top-10">
         <div className="flex justify-between items-center">
           <p>{title}</p>
           <img src="images/icon-ellipsis.svg" />
