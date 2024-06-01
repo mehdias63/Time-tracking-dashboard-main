65d9831160c934dbfc950368edce051ccbf019ff
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2024-06-01 15:19:18 +0330

    update app and Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import {useState} from 'react'
 import Card from './components/Card'
 
 
 function App() {
-  
+  const [time, setTime] = useState("Weekly") 
 
   return (
-   <div className='flex flex-col justify-center items-center gap-[6rem] max-w-[90rem] text-white py-10 font-rub'>
-    <div className='bg-dark-blue rounded-xl w-[20rem] h-[12.5rem] mb-[-4rem]'>
-      <div className='bg-dark-purple flex px-5 items-center w-[20rem] h-[8.5rem] rounded-xl'>
+   <main className='flex flex-col md:flex-row justify-center items-center md:items-start gap-[6rem] md:gap-4 max-w-[90rem] text-white py-10 font-rub mx-auto'>
+    <div className='bg-dark-blue rounded-xl w-[20rem] h-[12.5rem] md:h-[34rem] lg:h-[32rem] mb-[-4rem] lg:mb-4'>
+      <div className='bg-dark-purple flex md:flex-col px-5 items-center md:items-start md:p-9 w-[20rem] h-[8.5rem] md:h-[21rem] rounded-xl'>
         <img src='/images/image-jeremy.png' className='w-[4.875rem] border-4 rounded-full' />
-        <p className='ml-auto text-blue-sky text-[0.9375rem]'>Report for <span className='block text-2xl text-white'>Jeremy Robson</span></p>
+        <p className='ml-auto md:ml-0 text-blue-sky text-[0.9375rem] md:mt-12'>Report for <span className='block text-2xl text-white md:mr-[6rem] md:text-[2.5rem] md:mt-4 md:leading-10'>Jeremy Robson</span></p>
       </div>
-      <div className='flex justify-center items-center gap-8 mt-4 text-xl'>
-        <a>Daily</a>
-        <a>Weekly</a>
-        <a>Monthly</a>
+      <div className='flex md:flex-col justify-center items-center md:items-start md:ml-8 gap-8 mt-4 text-xl'>
+        <a
+        className={`a-item text-blue-sky ${time === "Daily" ? "active" : ""}`}
+        onClick={() => setTime("Daily")}
+        >Daily</a>
+        <a
+        className={`a-item text-blue-sky ${time === "Weekly" ? "active" : ""}`}
+        onClick={() => setTime("Weekly")}
+        >Weekly</a>
+        <a
+        className={`a-item text-blue-sky ${
+          time === "Monthly" ? "active" : ""
+        }`}
+        onClick={() => setTime("Monthly")}
+        >Monthly</a>
       </div>
     </div>
+    <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
     <Card
     color="#FF8B64"
     icon="/images/icon-work.svg"
@@ -60,7 +72,8 @@ function App() {
     time="2hrs"
     text="Last Week - 2hrs"
     />
-   </div>
+    </div>
+   </main>
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,19 +2,19 @@ import React from "react";
 
 export default function Card({ color, icon, title, text, time }) {
   return (
-    <div className="relative last:mb-20">
+    <div className="relative mb-20 md:mb-20 lg:mb-40">
       <div
         style={{ backgroundColor: color }}
-        className="w-[20rem] h-[6rem] rounded-xl"
+        className="w-[20rem] h-[6rem] md:w-[12.5rem] rounded-xl"
       >
         <img src={icon} className="ml-auto mr-4" />
       </div>
-      <div className="text-[#FFF] bg-dark-blue w-[20rem] h-[8rem] rounded-xl p-4 flex flex-col justify-center gap-6 absolute top-10">
+      <div className="text-[#FFF] bg-dark-blue w-[20rem] h-[8rem] md:w-[12.5rem] lg:h-[12.5rem] rounded-xl p-4 flex flex-col justify-center gap-6 absolute top-10">
         <div className="flex justify-between items-center">
           <p className="text-xl">{title}</p>
           <img src="images/icon-ellipsis.svg" />
         </div>
-        <div className="flex justify-between items-center">
+        <div className="flex md:flex-col justify-between items-center md:items-start lg:gap-2">
           <h2 className="text-[2rem]">{time}</h2>
           <p className="text-blue-sky text-[0.9375rem]">{text}</p>
         </div>
